457b2f698da6f8aa1f3bde521b8aec07897715ed
Nguyen Luong Toan <toannl@lqa.local>
2020-03-02 11:36:05 +0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            SearchingOnGoogle.httpsstackoverflowcomquestions8898182howtohandlekeypresseventinconsoleapplication();
+            Console.WriteLine("Bye World!");
+        }
+    
+
+        static void wl(string g)
+        {
+            Console.WriteLine(g);
+        }
+        
+    }
+}
